935f4643518b91e6dbd20d240cacb0f20b829060
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2022-09-13 10:00:39 +0400

    Add files via upload

---
+++ 1three.js
@@ -0,0 +1,9 @@
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+        return "Under 25";
+} 
+  if (val  < 55) {  // Change this line
+return "Under 55"}
+return "55 of Over";
+} 
+testLessThan(10)
