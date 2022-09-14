60d8c6c3aa60c27b876e831d279521409a0b62e0
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2022-09-14 09:44:57 +0400

    Add files via upload

---
+++ html.js
@@ -0,0 +1,24 @@
+<h2>CatPhotoApp</h2>
+<main>
+<div>
+  <p>Click here to view more <a href="#">cat photos</a>.</p>
+  <p>Things cats love:</p>
+<ul>
+    <li>cat nip</li>
+
+    <li>laser pointers</li>
+
+    <li>lasagna</li>
+</ul>
+  <p>Top 3 things cats hate:</p>
+<ol>    
+ <li>flea treatment</li>
+    <li>thunder</li>
+    <li>other cats</li>
+
+</ol>
+
+
+
+</div>
+</main>
